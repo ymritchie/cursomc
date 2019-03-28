1c81addb8dcedbd2bc4d6ad0d0ec29b40340b531
bbpf0415 <bbpf0415@BBPGETEC415.bbp.com.br>
2019-03-28 12:51:50 -0300

    Validação cusromizada: email repetido

--- src/main/java/com/ritchie/cursomc/domain/Cliente.java
+++ src/main/java/com/ritchie/cursomc/domain/Cliente.java
@@ -8,12 +8,15 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.ColumnDefault;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ritchie.cursomc.domain.enums.TipoCliente;
@@ -26,6 +29,8 @@ public class Cliente implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@Column(unique=true)
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
--- src/main/java/com/ritchie/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/ritchie/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ritchie.cursomc.domain.Cliente;
 import com.ritchie.cursomc.domain.enums.TipoCliente;
 import com.ritchie.cursomc.dto.ClienteNewDTO;
+import com.ritchie.cursomc.repositories.ClienteRepository;
 import com.ritchie.cursomc.resource.exceptions.FieldMessage;
 import com.ritchie.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido."));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente."));
 		}
 
 		for (FieldMessage e : list) {
