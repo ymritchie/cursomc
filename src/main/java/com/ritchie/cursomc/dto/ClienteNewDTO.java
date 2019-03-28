680d7cdeefe36a14391d9e33f882468d8a1505ce
bbpf0415 <bbpf0415@BBPGETEC415.bbp.com.br>
2019-03-28 10:32:02 -0300

    Validation do Cliente

--- src/main/java/com/ritchie/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/ritchie/cursomc/dto/ClienteNewDTO.java
@@ -2,21 +2,44 @@ package com.ritchie.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.ritchie.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preencimento Obrigatório!")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres.")
 	private String nome;
+	
+	@NotEmpty(message="Preencimento Obrigatório!")
+	@Email(message="Email inválido!")
 	private String email;
+	
+	@NotEmpty(message="Preencimento Obrigatório!")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message="Preencimento Obrigatório!")
 	private String logradouro;
+	@NotEmpty(message="Preencimento Obrigatório!")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Preencimento Obrigatório!")
 	private String cep;
 	
+	@NotEmpty(message="Preencimento Obrigatório!")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
---
+++ src/main/java/com/ritchie/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.ritchie.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.ritchie.cursomc.domain.enums.TipoCliente;
+import com.ritchie.cursomc.dto.ClienteNewDTO;
+import com.ritchie.cursomc.resource.exceptions.FieldMessage;
+import com.ritchie.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido."));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido."));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
