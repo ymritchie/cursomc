46e1857ba59b64ac9a7eaaf734a0e09d200bce01
Yanisley Mora Ritchie <ymritchie@gmail.com>
2019-04-22 22:12:38 -0300

    Formatado to String

--- src/main/java/com/ritchie/cursomc/domain/Pedido.java
+++ src/main/java/com/ritchie/cursomc/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.ritchie.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -142,11 +145,13 @@ public class Pedido implements Serializable {
 
 	@Override
 	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 		StringBuilder builder = new StringBuilder();
 		builder.append("Pedido número: ");
 		builder.append(getId());
 		builder.append(", Instante: ");
-		builder.append(getInstante());
+		builder.append(sf.format(getInstante()));
 		builder.append(", Cliente: ");
 		builder.append(getCliente().getNome());
 		builder.append(", Situação do Pagamento: ");
@@ -157,7 +162,7 @@ public class Pedido implements Serializable {
 		}
 		
 		builder.append("Valor Total: ");
-		builder.append(getValorTotal());		
+		builder.append(nf.format(getValorTotal()));		
 		return builder.toString();
 	}
 	
