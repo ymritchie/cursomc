2d50fc730aef8914c83b56d7eff60e3e25f47008
bbpf0415 <bbpf0415@BBPGETEC415.bbp.com.br>
2019-04-30 09:15:37 -0300

    Email HTML

--- src/main/java/com/ritchie/cursomc/services/EmailService.java
+++ src/main/java/com/ritchie/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.ritchie.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.ritchie.cursomc.domain.Pedido;
@@ -8,4 +10,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/ritchie/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/ritchie/cursomc/services/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.ritchie.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 	
@@ -13,6 +16,9 @@ public class SmtpEmailService extends AbstractEmailService {
 	@Autowired
 	private MailSender mailSender;
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando Email....");
@@ -20,5 +26,15 @@ public class SmtpEmailService extends AbstractEmailService {
 		LOG.info("Email enviado....");
 		
 	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando Email....");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado....");
+		
+	}
+	
+	
 
 }
