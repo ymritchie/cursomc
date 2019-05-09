40e4ae56c88f94a70d5ac9d7764653de7e1188c0
bbpf0415 <bbpf0415@BBPGETEC415.bbp.com.br>
2019-05-09 09:11:42 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/ritchie/cursomc/services/PedidoService.java
+++ src/main/java/com/ritchie/cursomc/services/PedidoService.java
@@ -1,12 +1,15 @@
 package com.ritchie.cursomc.services;
 
 import java.util.Date;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ritchie.cursomc.domain.Cliente;
 import com.ritchie.cursomc.domain.ItemPedido;
 import com.ritchie.cursomc.domain.PagamentoComBoleto;
 import com.ritchie.cursomc.domain.Pedido;
@@ -14,6 +17,8 @@ import com.ritchie.cursomc.domain.enums.EstadoPagamento;
 import com.ritchie.cursomc.repositories.ItemPedidoRepository;
 import com.ritchie.cursomc.repositories.PagamentoRepository;
 import com.ritchie.cursomc.repositories.PedidoRepository;
+import com.ritchie.cursomc.security.UserSS;
+import com.ritchie.cursomc.services.exceptions.AuthorizationException;
 import com.ritchie.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -73,6 +78,19 @@ public class PedidoService {
 		itemPedidoRepository.save(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		
+		return repository.findByCliente(cliente, pageRequest);
 	}
 	
 }
