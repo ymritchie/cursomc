24b5fbc160d874b32a9fc59b220e22db7c51803d
bbpf0415 <bbpf0415@BBPGETEC415.bbp.com.br>
2019-05-28 16:43:23 -0300

    Padronizando execoes

--- src/main/java/com/ritchie/cursomc/resource/exceptions/ValidationError.java
+++ src/main/java/com/ritchie/cursomc/resource/exceptions/ValidationError.java
@@ -7,18 +7,20 @@ public class ValidationError extends StandardError {
 
 	private static final long serialVersionUID = 1L;
 	
-	private List<FieldMessage> list = new ArrayList<>();
+	private List<FieldMessage> errors = new ArrayList<>();
 
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
+	
+	
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
 	}
 
 	public List<FieldMessage> getErrors() {
-		return list;
+		return errors;
 	}
 
 	public void addError(String fieldName, String message) {
-		list.add(new FieldMessage(fieldName, message));
+		errors.add(new FieldMessage(fieldName, message));
 	}
 
 }
